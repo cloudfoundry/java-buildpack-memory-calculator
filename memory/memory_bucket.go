05c28a4d682d194657d89b4ffb8cd773fbc7b9c7
Steve Powell <spowell@pivotal.io>
2015-04-28 14:25:13 +0100

    Add memory_bucket.go which defines the Bucket and StackBucket interfaces.
    A Bucket is a range of supported memory sizes together with a weight used to
    determine relative proportions of the total memory to use. Buckets are used to
    help calculate memory allocations for a JRE based upon an overall memory
    allocation and optional constraints. StackBucket is a specialisation of
    Bucket which also knows an individual stack allocation default.
    
    [#93189380]

---
+++ memory/memory_bucket.go
@@ -0,0 +1,107 @@
+// Encoding: utf-8
+// Cloud Foundry Java Buildpack
+// Copyright (c) 2015 the original author or authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package memory
+
+import (
+	"fmt"
+	"strings"
+)
+
+// This file defines and implements the Bucket interface type.
+// A Bucket is used to calculate default sizes for various type of memory.
+
+type Bucket interface {
+	GetKSize() *int64 // Size of bucket in kilobytes, if set; nil otherwise
+	SetKSize(int64)
+
+	GetRange() Range // Permissible range of sizes for this bucket.
+	SetRange(Range)
+
+	GetWeight() float64 // Proportion of total memory this bucket is allowed to consume by default.
+}
+
+type bucket struct {
+	name   string
+	ksize  *int64
+	srange Range
+	weight float64
+}
+
+// Returns a pointer to int64 because this value can be unset.
+func (b *bucket) GetKSize() *int64 {
+	return b.ksize
+}
+
+// Generates a new pointer to int64 internally.
+func (b *bucket) SetKSize(ksize int64) {
+	tmpsize := ksize
+	b.ksize = &tmpsize
+}
+
+func (b *bucket) GetRange() Range {
+	return b.srange
+}
+
+func (b *bucket) SetRange(srange Range) {
+	b.srange = srange
+}
+
+func (b *bucket) GetWeight() float64 {
+	return b.weight
+}
+
+func NewBucket(name string, weight float64, srange Range) (Bucket, error) {
+	return newBucket(name, weight, srange)
+}
+
+func newBucket(name string, weight float64, srange Range) (*bucket, error) {
+	name = strings.TrimSpace(name)
+	if name == "" {
+		return nil, fmt.Errorf("Bucket name must not be blank.")
+	}
+	if weight < 0.0 || weight > 1.0 {
+		return nil, fmt.Errorf("Weight (%g) for bucket %s must be <=1 and >=0.", weight, name)
+	}
+	return &bucket{
+		name:   name,
+		srange: srange,
+		weight: weight,
+	}, nil
+}
+
+// A StackBucket is exactly like a bucket except that it supports DefaultSize().
+type StackBucket interface {
+	Bucket
+	DefaultSize() *MemSize
+}
+
+func NewStackBucket(weight float64, srange Range) (StackBucket, error) {
+	return newBucket("stack", weight, srange)
+}
+
+var (
+	jvm_DEFAULT_STACK_SIZE = NewMemSize(mEGA)
+)
+
+// The default stacksize (minimum of the range, or the JRE standard default).
+func (b *bucket) DefaultSize() *MemSize {
+	floor := b.srange.Floor()
+	if floor.Bytes() == 0 {
+		return jvm_DEFAULT_STACK_SIZE
+	}
+	return floor
+}
---
+++ memory/memory_bucket_test.go
@@ -0,0 +1,109 @@
+// Encoding: utf-8
+// Cloud Foundry Java Buildpack
+// Copyright (c) 2015 the original author or authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package memory_test
+
+import (
+	"github.com/cloudfoundry/java-buildpack-memory-calculator/memory"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("MemoryBucket and StackMemoryBucket", func() {
+	Context("constructors", func() {
+		var (
+			DEFAULT_JRE_STACK_SIZE = memory.NewMemSize(mEGA)
+			testRange              memory.Range
+			testZeroRange          memory.Range
+			shouldFail             func(memory.Bucket, error)
+			shouldWork             func(memory.Bucket, error) memory.Bucket
+		)
+
+		BeforeEach(func() {
+			var err error
+			testRange, err = memory.NewRangeFromString("2m..3m")
+			Ω(err).ShouldNot(HaveOccurred())
+			testZeroRange, err = memory.NewRangeFromString("0..3m")
+			Ω(err).ShouldNot(HaveOccurred())
+
+			shouldFail = func(b memory.Bucket, err error) {
+				Ω(b).Should(BeNil())
+				Ω(err).Should(HaveOccurred())
+			}
+
+			shouldWork = func(b memory.Bucket, err error) memory.Bucket {
+				Ω(b).ShouldNot(BeNil())
+				Ω(err).ShouldNot(HaveOccurred())
+				return b
+			}
+		})
+
+		Context("works", func() {
+			It("with non-blank name and good weights", func() {
+				b := shouldWork(memory.NewBucket("abucketname", 0.0, testRange))
+				Ω(b.GetRange()).Should(Equal(testRange))
+				Ω(b.GetKSize()).Should(BeNil())
+				b.SetKSize(124)
+				Ω(*b.GetKSize()).Should(Equal(int64(124)))
+				Ω(b.GetWeight()).Should(BeNumerically("~", 0.0))
+			})
+
+			It("with StackBucket and good weights", func() {
+				sb, err := memory.NewStackBucket(0.0, testZeroRange)
+				Ω(err).ShouldNot(HaveOccurred())
+
+				Ω(sb.GetRange()).Should(Equal(testZeroRange))
+				Ω(sb.GetKSize()).Should(BeNil())
+				sb.SetKSize(124)
+				Ω(*sb.GetKSize()).Should(Equal(int64(124)))
+				Ω(sb.GetWeight()).Should(BeNumerically("~", 0.0))
+				Ω(sb.DefaultSize()).Should(Equal(DEFAULT_JRE_STACK_SIZE))
+
+			})
+
+			It("the same with spaced non-blank name", func() {
+				b := shouldWork(memory.NewBucket("  \t abucketname ", 0.0, testRange))
+				Ω(shouldWork(memory.NewBucket("abucketname", 0.0, testRange))).Should(Equal(b))
+			})
+
+			It("with non-zero weights", func() {
+				b := shouldWork(memory.NewBucket("abucketname", 1.0, testRange))
+				Ω(b.GetWeight()).Should(BeNumerically("~", 1.0))
+				b = shouldWork(memory.NewBucket("abucketname", 0.2, testRange))
+				Ω(b.GetWeight()).Should(BeNumerically("~", 0.2))
+				b = shouldWork(memory.NewBucket("abucketname", 0.9, testRange))
+				Ω(b.GetWeight()).Should(BeNumerically("~", 0.9))
+			})
+		})
+
+		Context("fails", func() {
+			It("fails with blank names", func() {
+				shouldFail(memory.NewBucket("", 0.0, testRange))
+				shouldFail(memory.NewBucket("   ", 0.0, testRange))
+				shouldFail(memory.NewBucket("  \t", 0.0, testRange))
+			})
+
+			It("fails with bad weights", func() {
+				shouldFail(memory.NewBucket("abucket", -0.01, testRange))
+				shouldFail(memory.NewBucket("abucket", 10.0, testRange))
+				shouldFail(memory.NewBucket("abucket", 1.01, testRange))
+				shouldFail(memory.NewStackBucket(-0.01, testRange))
+				shouldFail(memory.NewStackBucket(10.0, testRange))
+				shouldFail(memory.NewStackBucket(1.01, testRange))
+			})
+		})
+	})
+})
