5148668b07108b97ba55233bdc5eb432f668468a
Ben Hale <bhale@pivotal.io>
2019-05-01 16:44:20 -0700

    Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- ci/create-release.sh
+++ ci/create-release.sh
@@ -9,14 +9,12 @@ update_version() {
   sed -E -i '' "s|(^[ ]*VERSION:[ ]*).+$|\1${1}|" ci/package.yml
 }
 
-git branch -f release
-git checkout release
 update_version ${RELEASE}
 git add .
 git commit --message "v${RELEASE} Release"
 git tag -s v${RELEASE} -m "v${RELEASE}"
 
-git master
+git reset --hard HEAD^1
 update_version ${SNAPSHOT}
 git add .
 git commit --message "v${SNAPSHOT} Development"
