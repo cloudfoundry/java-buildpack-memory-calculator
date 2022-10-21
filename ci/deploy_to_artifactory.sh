0d0697d4f84033e3adfb735e061a3e239234cb2a
Torsten Kohn <tkohn@users.noreply.github.com>
2022-10-21 07:56:28 +0200

    Publish artefacts to new Artifactory

--- ci/deploy_to_artifactory.sh
+++ ci/deploy_to_artifactory.sh
@@ -2,16 +2,30 @@
 
 set -euo pipefail
 
-readonly filename="${1}"
+readonly filepath="${1}"
 readonly architecture="${2}"
 readonly version="${3}"
 
 mvn deploy:deploy-file \
-	-Durl=https://artifact.instana.io/artifactory/agent-releases  \
-	-Dfile=${filename} \
-	-DrepositoryId=agent-releases \
-	-DgroupId=com.instana \
-	-DartifactId=memory_calculator \
-    -Dclassifier=${architecture}\
-	-Dpackaging=bin \
-	-Dversion=${version}
+  -Durl=https://artifact.instana.io/artifactory/agent-releases \
+  -Dfile="${filepath}" \
+  -DrepositoryId=agent-releases \
+  -DgroupId=com.instana \
+  -DartifactId=memory_calculator \
+  -Dclassifier="${architecture}" \
+  -Dpackaging=bin \
+  -Dversion="${version}"
+
+set +x
+readonly base_url="${DELIVERY_ARTIFACTORY_GENERIC_BASE_URL}"
+readonly org_path=com/instana
+readonly artifact_id="memory_calculator"
+readonly target_url="${base_url}/${org_path}/${artifact_id}/${version}/${artifact_id}-${version}-${architecture}"
+echo -e "[\e[1m\e[34mINFO\e[39m\e[21m] Uploading \e[92m$target_url\e[39m"
+readonly status_code=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -u "${DELIVERY_ARTIFACTORY_USERNAME}:${DELIVERY_ARTIFACTORY_PASSWORD}" -X PUT "$target_url" -T "${filepath}")
+echo # curl doesn't output a line break on error
+if test "$status_code" -ne 201; then
+  echo -e "[\e[1m\e[91mERROR\e[39m\e[21m] curl returned $status_code, exiting..."
+  exit 1
+fi
+set -x
--- go.mod
+++ go.mod
@@ -6,5 +6,10 @@ require (
 	github.com/onsi/gomega v1.18.1
 	github.com/sclevine/spec v1.4.0
 	github.com/spf13/pflag v1.0.5
+)
+
+require (
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
